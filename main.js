8e36acfb12a041f119e2745f3c2b0448a2ff4c12
Suman Chakraborty <suchakra012@hotmail.com>
2018-05-05 16:43:18 +0530

    Updated source code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Class, Welcome to Docker session\n");
+  response.end("Hello to EVERYONE !!!!!!!\n");
 });
 
 // listen on localhost:8000
