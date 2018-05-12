7c390fa78c5f181c7d30012b00bef55c1942032d
Suman Chakraborty <suchakra012@hotmail.com>
2018-05-12 11:59:44 +0530

    Rollback the change

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("HELLO , WELCOME TO THE DOCKER & KUBERNETES CLASS\n");
+  response.end("HELLO , To EVERYONE\n");
 });
 
 // listen on localhost:8000
