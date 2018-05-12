b05ec1452fab020db6b4d8429b43d99692e0991c
Suman Chakraborty <suchakra012@hotmail.com>
2018-05-12 11:49:16 +0530

    Updated the source code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello to EVERYONE !!!!!!!\n");
+  response.end("HELLO , WELCOME TO THE DOCKER & KUBERNETES CLASS\n");
 });
 
 // listen on localhost:8000
